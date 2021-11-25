f4b9f0acbc41cc989af81628c0d0ed48c2bdb324
J. Blumenkamp <jb2270@cam.ac.uk>
2021-11-25 23:56:57 +0000

    Use type conversions for serialization, make shorter

--- inc/robomaster_protocol.hpp
+++ inc/robomaster_protocol.hpp
@@ -18,7 +18,7 @@ constexpr static uint16_t CRC_PACKAGE_INIT = 13970;
 class package {
 public:
     package() : _id{0}, _count{0}, _data{} {}
-    package(unsigned int id, std::vector<uint8_t>& data) : _id{id}, _count{0}, _data{data} {}
+    package(uint16_t id, std::vector<uint8_t>& data) : _id{id}, _count{0}, _data{data} {}
 
     auto get_id() const
     {
@@ -40,8 +40,8 @@ public:
 
 private:
 
-    unsigned int _id;
-    unsigned int _count;
+    uint16_t _id;
+    uint16_t _count;
     std::vector<uint8_t> _data;
 };
 
@@ -87,20 +87,17 @@ std::ostream& operator<<(std::ostream& out, const package& pkg)
     const auto crc_header = crc::compute_crc8(CRC_HEADER_INIT, serial_data);
     serial_data.push_back(crc_header);
 
-    serial_data.insert(serial_data.end(), {
-        static_cast<uint8_t>(pkg._id & 0xff),
-        static_cast<uint8_t>((pkg._id & 0xff00) >> 8),
-        static_cast<uint8_t>(id_count & 0xff),
-        static_cast<uint8_t>((id_count & 0xff00) >> 8),
-    });
+    auto pkg_id_b = reinterpret_cast<const uint8_t*>(&pkg._id);
+    serial_data.insert(serial_data.end(), pkg_id_b, pkg_id_b+2);
+    auto id_count_b = reinterpret_cast<const uint8_t*>(&id_count);
+    serial_data.insert(serial_data.end(), id_count_b, id_count_b+2);
 
     serial_data.insert(serial_data.end(), pkg._data.begin(), pkg._data.end());
 
     const auto crc_package = crc::compute_crc16(CRC_PACKAGE_INIT, serial_data);
-    serial_data.insert(serial_data.end(), {
-        static_cast<uint8_t>(crc_package & 0xff),
-        static_cast<uint8_t>((crc_package & 0xff00) >> 8),
-    });
+    auto crc_package_b = reinterpret_cast<const uint8_t*>(&crc_package);
+    serial_data.insert(serial_data.end(), crc_package_b, crc_package_b+2);
+
     return out.write(reinterpret_cast<char*>(serial_data.data()), serial_data.size());
 }
 
@@ -108,28 +105,15 @@ std::istream& operator>>(std::istream& in, package& pkg)
 {
     while(true)
     {
-        uint8_t magic_number_a = 0;
-        while(magic_number_a != MAGIC_NUMBER_A) {
-            in.read(reinterpret_cast<char*>(&magic_number_a), 1);
-        }
-
-        uint8_t length_parsed;
-        in.read(reinterpret_cast<char*>(&length_parsed), 1);
-
-        uint8_t magic_number_b;
-        in.read(reinterpret_cast<char*>(&magic_number_b), 1);
+        while(in.get() != MAGIC_NUMBER_A);
+        uint8_t length_parsed = in.get();
+        uint8_t magic_number_b = in.get();
         if (magic_number_b != MAGIC_NUMBER_B) {
             continue;
         }
 
-        uint8_t crc_header_parsed;
-        in.read(reinterpret_cast<char*>(&crc_header_parsed), 1);
-
-        std::vector<uint8_t> data{
-            MAGIC_NUMBER_A,
-            length_parsed,
-            MAGIC_NUMBER_B
-        };
+        uint8_t crc_header_parsed = in.get();
+        std::vector<uint8_t> data{MAGIC_NUMBER_A, length_parsed, MAGIC_NUMBER_B};
         const auto crc_header_expected = crc::compute_crc8(CRC_HEADER_INIT, data);
 
         if (crc_header_parsed != crc_header_expected) {
@@ -141,16 +125,16 @@ std::istream& operator>>(std::istream& in, package& pkg)
 
         uint16_t crc_package_parsed;
         in.read(reinterpret_cast<char*>(&crc_package_parsed), 2);
-        // Finished parsing, verify package checksum
 
+        // Finished parsing, verify package checksum
         const auto crc_package_expected = crc::compute_crc16(CRC_PACKAGE_INIT, data);
         if (crc_package_parsed != crc_package_expected) {
             continue;
         }
 
         // now move into package data
-        pkg._id = data[4] | ((static_cast<uint16_t>(data[5]) << 8) & 0xff00);
-        pkg._count = data[6] | ((static_cast<uint16_t>(data[7]) << 8) & 0xff00);
+        pkg._id = *(reinterpret_cast<uint16_t*>(&data[4]));
+        pkg._count = *(reinterpret_cast<uint16_t*>(&data[6]));
         pkg._data.assign(data.begin() + 8, data.end());
 
         return in;
