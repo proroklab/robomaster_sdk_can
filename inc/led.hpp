7c3b2fe1117afcd9a2528d8154d91334cad13051
J. Blumenkamp <jb2270@cam.ac.uk>
2021-11-29 20:32:52 +0000

    Add LED

---
+++ inc/led.hpp
@@ -0,0 +1,38 @@
+#include <vector>
+
+#include <robomaster_protocol.hpp>
+
+namespace robomaster
+{
+namespace command
+{
+class led
+{
+public:
+    led(std::iostream& io)
+        : _io{io}
+    {}
+
+    void send_led(uint8_t mode, uint8_t r, uint8_t g, uint8_t b, uint16_t speed_up, uint16_t speed_down, uint8_t led_mask)
+    {
+        robomaster::package pkg{0x09, 0x18, 0x3f, 0x32, false, false};
+
+        pkg << mode;
+        pkg << static_cast<uint8_t>(0xff);
+        pkg << static_cast<uint8_t>(0);
+        pkg << r << g << b;
+        pkg << static_cast<uint8_t>(0);
+        pkg << speed_up << speed_down;
+        pkg << led_mask;
+        pkg << static_cast<uint8_t>(0);
+
+        pkg.write_to(_io);
+    }
+
+private:
+    std::iostream& _io;
+};
+
+}
+}
+
